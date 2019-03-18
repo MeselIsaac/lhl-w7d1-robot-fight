29edf15db0f0a058f6355b7a6428e0af5e12c6cc
David Mills <dave@1morepx.com>
2019-03-18 15:28:26 -0400

    protyped a single turn

--- game.rb
+++ game.rb
@@ -4,17 +4,43 @@ require_relative 'robot'
 class Game
 
   def initialize
-    
+    robot1 = Robot.new('Bender')
+    robot2 = Robot.new('Wall-E')
+    robot3 = Robot.new('Karen (From SpongeBob, aka Plankton\'s wife)')
+
+    @robots = [robot1, robot2, robot3]
   end
 
   # This method will handle the main game loop
   def play
+    header "Round 1"
     
+    attacker = @robots[0]
+    defender = @robots[1]
+
+    puts "#{attacker.name} is attacking #{defender.name}!"
+
+    amount = attacker.attack defender
+
+    puts "#{attacker.name} does #{amount} damage!"
+
+    header "Summary"
+
+    puts game_summary
   end
 
   # methods declared after here will be marked as "private" 
   # i.e. they are not available outside of this class
   private
 
+  def header(message)
+    puts
+    puts "===== #{message} ====="
+    puts
+  end
+
+  def game_summary
+    @robots.map { |r| r.summary }.join("\n")
+  end
   
 end
--- robot.rb
+++ robot.rb
@@ -8,4 +8,20 @@ class Robot
     @hp = MAX_HEALTH
     @ap = 10
   end
+
+  def attack(other_robot)
+    damage = rand(@ap) + 1
+    other_robot.take_damage(damage)
+
+    damage
+  end
+
+  def take_damage(amount)
+    @hp -= amount
+  end
+
+  def summary
+    "#{@name}: #{@hp}HP"
+  end
+  
 end
